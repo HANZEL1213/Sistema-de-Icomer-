34652dc91ec94aa3643580a5c6bd3af443a7e932
hsanabria <145887817+HANZEL1213@users.noreply.github.com>
2026-07-06 12:57:00 -0600

    Hanzel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,12 +13,39 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'cliente' => \App\Http\Middleware\ClienteMiddleware::class,
         ]);
+
+        // <-- AGREGA ESTO
+        $middleware->redirectGuestsTo(fn () => route('admin.login'));
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Exceptions;
+// use Illuminate\Foundation\Configuration\Middleware;
+
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         health: '/up',
+//     )
+//     ->withMiddleware(function (Middleware $middleware): void {
+        
+//         $middleware->alias([
+//             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+//             'cliente' => \App\Http\Middleware\ClienteMiddleware::class,
+//         ]);
+//     })
+//     ->withExceptions(function (Exceptions $exceptions): void {
+//         //
+//     })->create();
